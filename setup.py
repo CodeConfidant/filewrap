4995d4f62c495334118f9a6f19fb51e1d9f88e23
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-04-09 17:50:29 -0400

    Prepared module for packaging.
    New directory:
    1. dist
    
    New File:
    1. setup.py
    
    New Archive:
    1. filewrap-0.1.tar.gz

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='filewrap',
+    version='0.1',
+    py_modules=['filewrap'],
+    license='MIT',
+    description='A Python module that wraps functionality from the os module for file system manipulation and management.',
+    long_description=open('README.md').read(),
+    url='https://github.com/CodeConfidant/filewrap-os',
+    author='Drew Hainer',
+    author_email='codeconfidant@gmail.com'
+)
