19228c7fde19aa07eff91e52f97973b015ac6045
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2021-09-19 20:17:55 -0400

    Updated size method to use os.path.getsize().
    - Updated test.py

--- tests/test.py
+++ tests/test.py
@@ -8,6 +8,7 @@ import filewrap
 
 dirpath = str("demo-renamed")
 filepath = str(dirpath + "/demo.txt")
+filepathcopy = str(dirpath + "/democopy.txt")
 
 alpha_lines = list([ 
     "This is line 1 of the demo.txt file.", 
@@ -29,12 +30,24 @@ gamma_lines = list([
 
 filewrap.mkdir("demo")
 filewrap.ren("demo", dirpath)
+
 filewrap.mkfile("t", filepath)
+filewrap.mkfile("t", filepathcopy)
+
 filewrap.writelines(filepath, alpha_lines, beta_lines, "This is line 7 of the demo.txt file.", "This is line 8 of the demo.txt file.")
+filewrap.writelines(filepathcopy, alpha_lines, beta_lines, "This is line 7 of the demo.txt file.", "This is line 8 of the demo.txt file.")
+
 filewrap.appendlines(filepath, gamma_lines, "This is line 12 of the demo.txt file.", "This is line 13 of the demo.txt file.")
+filewrap.appendlines(filepathcopy, gamma_lines, "This is line 12 of the demo.txt file.", "This is line 13 of the demo.txt file.")
+
 filewrap.tar_wrap(dirpath)
 filewrap.zip_wrap(dirpath)
+
+print(f"Size of {filepath}:", filewrap.size(filepath), "Bytes")
+print(f"Size of {dirpath}:", filewrap.size(dirpath), "Bytes")
+
 filewrap.rpdir()
-filewrap.rpfile("t", filepath)
+filewrap.rpfile("t", filepath, filepathcopy)
+
 filewrap.rmfile(dirpath + ".tar.gz", dirpath + ".zip")
 filewrap.rmall(dirpath)
